35c68893d6e3758fcbcc4605ac4d6ab03f2206e8
AliiMuhammed <mohamedtaha8000@gmail.com>
2024-03-06 20:36:22 +0200

    add notfound component

--- Clinet/teachable/src/Router.jsx
+++ Clinet/teachable/src/Router.jsx
@@ -1,10 +1,19 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "./App";
+import Admin from "./Pages/Admin/Admin";
+import Notfound from "./Pages/NotFound/Notfound";
 
 export const routes = createBrowserRouter([
   {
     path: "/",
     element: <App />,
     children: [],
+    errorElement: <Notfound />,
+  },
+  {
+    path: "/admin",
+    element: <Admin />,
+    children: [],
+    errorElement: <Notfound />,
   },
 ]);
